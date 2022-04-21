d4821d673d27170f59a27131e3b319cafb686f7e
zhaomeng <zhaomeng@testbird.com>
2022-04-21 10:39:08 +0800

    init logo

--- app/src/main/java/com/tools/android/translator/ui/LoadingActivity.kt
+++ app/src/main/java/com/tools/android/translator/ui/LoadingActivity.kt
@@ -1,7 +1,13 @@
 package com.tools.android.translator.ui
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.tools.android.translator.base.BaseBindingActivity
 import com.tools.android.translator.databinding.ActivityLoadingBinding
+import com.tools.android.translator.ui.translate.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * Created on 2022/4/20
@@ -13,5 +19,18 @@ class LoadingActivity: BaseBindingActivity<ActivityLoadingBinding>() {
         return ActivityLoadingBinding.inflate(layoutInflater)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            delay(2800L)
+            if (isPaused()) return@launch
+            enterMain()
+        }
+    }
 
+    private fun enterMain() {
+         val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
